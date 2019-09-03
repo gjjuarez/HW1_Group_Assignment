8b13b2837f67cf20facb64ac03f130d0262758ae
Brian Cardiel <42951440+bacardiel05@users.noreply.github.com>
2019-09-02 20:44:42 -0600

    Update done by Student B

--- Passphrase1.java
+++ Passphrase1.java
@@ -1,15 +1,16 @@
+// This is an Update done by Student B
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 public class Passphrase1 {
 	public static void main(String[] args) throws Exception {
-        System.out.println("This first trigger is a test to see if you are worthy to save the world!");
+        System.out.println("1st test to see if you are worthy");
         trigger1();
-        System.out.println("You have proved your worthiness!!\n");
+        System.out.println("You are worthy\n");
         System.out.println("Welcome reverser. Recently I discovered that a malicious mad scientist ");
-        System.out.println("will release the T-virus into the world. Our only hope is to diffuse the triggering mechanism by entering a");
-        System.out.println("5 passphrase sequence. The world is in danger unless you (yes you, the cs5375 student) discover the ");
-        System.out.println("5 passphrases before all of earth is taken over by flesh eating zombies... :(");
+        System.out.println("will release the Z-virus into the world. Our only hope is to diffuse the triggering mechanism by entering a");
+        System.out.println("5 passphrase sequence. The world is in danger unless you (yes you, the cs5375 slave) discover the ");
+        System.out.println("5 passphrases before all of earth is taken over by flesh eating chickens... :(");
         System.out.println();
         System.out.println("Please help! Now what about the 2nd?");
 	}
@@ -30,6 +31,7 @@ public class Passphrase1 {
 	
 	 public static void worldEnds() {
 	        System.out.println("Wrong. The earth has been destroyed!");
+         System.out.println("You have failed Everyone");
 	        System.exit(0);
 	    }
 }
